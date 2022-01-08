daa0626f90b98d77d9306087d0ee0efc31b489e5
Frank Mayer <frank.mayer1907@protonmail.com>
2022-01-08 15:59:25 +0100

    fixed package.json

--- commonjs-build.cjs
+++ commonjs-build.cjs
@@ -7,14 +7,20 @@ const execOptions = { stdio: "inherit" };
 const packageJson = JSON.parse(fs.readFileSync("./package.json").toString());
 try {
   packageJson.type = "commonjs";
-  fs.writeFileSync("./package.json", JSON.stringify(packageJson, null, 2));
+  fs.writeFileSync(
+    "./package.json",
+    JSON.stringify(packageJson, null, 2) + "\n"
+  );
 
   execSync("yarn run tsc -p ./cjsconfig.jsonc", execOptions);
 
   execSync("yarn run jest --coverage --verbose", execOptions);
 
   packageJson.type = "module";
-  fs.writeFileSync("./package.json", JSON.stringify(packageJson, null, 2));
+  fs.writeFileSync(
+    "./package.json",
+    JSON.stringify(packageJson, null, 2) + "\n"
+  );
 } catch (err) {
   if (err.toString) {
     console.error(err.toString());
@@ -23,7 +29,10 @@ try {
   }
 
   packageJson.type = "module";
-  fs.writeFileSync("./package.json", JSON.stringify(packageJson, null, 2));
+  fs.writeFileSync(
+    "./package.json",
+    JSON.stringify(packageJson, null, 2) + "\n"
+  );
 
   exit(1);
 }
